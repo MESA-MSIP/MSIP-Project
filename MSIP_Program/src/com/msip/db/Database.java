59dda08f5451a6b49f62350a64d386aacbeca162
festevez10 <estevezfernando1995@gmail.com>
2015-11-24 23:08:46 -0800

    more DB methods.

--- MSIP_Program/src/com/msip/db/Database.java
+++ MSIP_Program/src/com/msip/db/Database.java
@@ -5,34 +5,75 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class Database {
-	public static  Connection getConnection(){
-		try{
-			//Connects to your mySQL database using the url, username, and password.
-		Connection myConnection = (Connection) DriverManager.getConnection(Global.URL, Global.USERNAME, Global.PASSWORD);
-		return myConnection;
-		}catch(Exception e){
+	public static Connection getConnection() {
+		try {
+			// Connects to your mySQL database using the url, username, and
+			// password.
+			Connection myConnection = (Connection) DriverManager.getConnection(
+					Global.URL, Global.USERNAME, Global.PASSWORD);
+			return myConnection;
+		} catch (Exception e) {
 			System.out.println(e);
-			
+
 		}
 		return null;
 	}
 
-	public static void insertToTable(String firstName, String lastName){
-		try{
-			Connection myConnection = getConnection();
-			//Inserts a first and last namer to the  table using a SQL cmd.
-			//Don't forget to put single quotes around your string. Otherwise you will get an error.
-			PreparedStatement insert = (PreparedStatement) myConnection.prepareStatement("INSERT INTO myTable VALUE( NULL, '"+ firstName+"','"+lastName+"', NOW());");			
-			//Executes the cmd.
-			insert.executeUpdate();
-			}catch(Exception e){
-				System.out.println(e);
-			}
-		
+	public static void createTable() {
+
 	}
-	
-	public static void getReport(){
-		
+
+	public static void insertToTable(String firstName, String lastName) {
+		try {
+			Connection myConnection = getConnection();
+			// Inserts a first and last namer to the table using a SQL cmd.
+			// Don't forget to put single quotes around your string. Otherwise
+			// you will get an error.
+			PreparedStatement insert = (PreparedStatement) myConnection
+					.prepareStatement("INSERT INTO myTable VALUE( NULL, '"
+							+ firstName + "','" + lastName + "', NOW());");
+			// Executes the cmd.
+			insert.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+	public static void saveTable() {
+
+	}
+
+	public static void deleteTable() {
+
+	}
+
+	public static void addStudent() {
+
+	}
+
+	public static void deleteStudent() {
+
+	}
+
+	public static void addAdmin() {
+
+	}
+
+	public static void deleteAdmin() {
+
+	}
+
+	public static void getLoginTableEntry() {
+
+	}
+
+	public static void getDailyReport() {
+
+	}
+
+	public static void getReport() {
+
 	}
 
 }
