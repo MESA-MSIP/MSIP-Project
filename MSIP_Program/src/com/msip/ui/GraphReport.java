48121340d54a56243689f39675ea2f5a65cef493
Celina <Celina@198.105.254.228>
2016-01-10 22:36:13 -0800

    Adding new class for the graphs
    Signed-off-by: Celina <Celina@198.105.254.228>

---
+++ MSIP_Program/src/com/msip/ui/GraphReport.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.msip.ui;
+
+/**
+ * @author Celina
+ *
+ */
+public class GraphReport {
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
