d5221f76ff8713d631b0cfd597f23ea98c5c1e6b
festevez10 <estevezfernando1995@gmail.com>
2016-02-05 01:18:06 -0800

    Generate randome data in csv class.

--- MSIP_Program/src/com/msip/external/ReportMakerCSV.java
+++ MSIP_Program/src/com/msip/external/ReportMakerCSV.java
@@ -3,7 +3,12 @@ package com.msip.external;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.swing.JFileChooser;
+
 import com.itextpdf.text.Document;
 
 public class ReportMakerCSV {
@@ -64,6 +69,12 @@ public class ReportMakerCSV {
 		}
 
 	}
+	private static Timestamp getRandomDate() {
+		long offset = Timestamp.valueOf("2015-01-01 00:00:00").getTime();
+		long end = Timestamp.valueOf("2016-01-01 00:00:00").getTime();
+		long diff = end - offset + 1;
+		return new Timestamp(offset + (long) (Math.random() * diff));
+	}
 
 	public static void main(String[] args) throws IOException {
 		JFileChooser fc = new JFileChooser();
@@ -75,13 +86,50 @@ public class ReportMakerCSV {
 			System.out.println(yourFolder);
 //TODO Change to csv.
 			File pathToPDF = new File(yourFolder.getAbsolutePath()
-					+ File.separator + "newreport.csv");
+					+ File.separator + "Samplereport_csv.csv");
 
 			ReportMakerCSV csv = new ReportMakerCSV(pathToPDF);
-			String[] dates = { "1/3/16", "1/4/16", "1/5/16" };
+			   SimpleDateFormat str = new SimpleDateFormat("yyyy-MM-dd");
 
+			
+			Date[] d = new Date[5];
+			Timestamp timestamp;
+			for (int i = 0; i < d.length; i++) {
+				timestamp = getRandomDate();
+				d[i] = new Date(timestamp.getTime());
+			}
+			String[] s = new String[5];
+			for(int i = 0; i < s.length; i++){
+				s[i] = str.format(d[i]);
+			}
+			
+			Date[] d1= new Date[7];
+			Timestamp timestamp1;
+			for (int i = 0; i < d1.length; i++) {
+				timestamp1 = getRandomDate();
+				d1[i] = new Date(timestamp1.getTime());
+			}
+			String[] s2 = new String[5];
+			for(int i = 0; i < s2.length; i++){
+				s2[i] = str.format(d1[i]);
+			}
+			
+			Date[] d2 = new Date[9];
+			Timestamp timestamp2;
+			for (int i = 0; i < d2.length; i++) {
+				timestamp2 = getRandomDate();
+				d2[i] = new Date(timestamp2.getTime());
+			}
+			String[] s3 = new String[5];
+			for(int i = 0; i < s3.length; i++){
+				s3[i] = str.format(d2[i]);
+			}
+			
 			csv.addHeader("Name,times Present,Dates present");
-			csv.CreateCSVFile("Fernando", "3", dates);
+			csv.CreateCSVFile("Fernando Estevez", "5", s);
+			csv.CreateCSVFile("Christian Martinez", "7", s2);
+			//csv.CreateCSVFile("Celina Lazaro", "9", s3);
+
 
 		}
 	}
