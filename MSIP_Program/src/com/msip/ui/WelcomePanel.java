23f2c705aaee6e81224c05ac86ef244c2c5310f2
Chris <Chris@Laptop>
2016-02-24 19:11:05 -0800

    WelcomePanel: Layout Finished

--- MSIP_Program/src/com/msip/ui/WelcomePanel.java
+++ MSIP_Program/src/com/msip/ui/WelcomePanel.java
@@ -26,19 +26,29 @@ public class WelcomePanel extends JPanel
 		setBounds(new Rectangle(0, 0, 800, 480));
 		setLayout(new BorderLayout(0, 0));
 		
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("Message");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Timer\r\n");
+		panel.add(lblNewLabel_1);
+		
 		JPanel welcomeCards = new JPanel();
 		welcomeCards.setLayout(new CardLayout(0, 0));
 		
 		
 		//Construct Cards
 		JPanel StudentSurveyPanel = new StudentSurveyPanel(manager);
+		JPanel NotificationsPanel = new NotificationsPanel(manager);
 		
 		//Add the Cards to the JPanel
 		welcomeCards.add(StudentSurveyPanel, GlobalUI.StudentSurveyPanel);
+		welcomeCards.add(NotificationsPanel, GlobalUI.NotificationsPanel);
 		add(welcomeCards, BorderLayout.CENTER);
-		
-		JPanel panel = new JPanel();
-		add(panel, BorderLayout.NORTH);
 		
 		
 		
