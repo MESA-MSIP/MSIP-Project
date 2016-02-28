57d364301b0cdc14ee65856bd33d652edf28a289
festevez10 <estevezfernando1995@gmail.com>
2016-02-27 19:02:41 -0800

    Finish implementing the JDateChooser to the reportPanel class.

--- MSIP_Program/src/com/msip/db/NotificationTable.java
+++ MSIP_Program/src/com/msip/db/NotificationTable.java
@@ -1,25 +1,55 @@
 package com.msip.db;
 
 import java.sql.SQLException;
-
+import java.util.Date;
 import com.mysql.jdbc.PreparedStatement;
 
 public class NotificationTable {
+	
+	/**
+	 * Creates Notification Table if it doesn't exist.
+	 */
 	public NotificationTable() {
 		try {
 			PreparedStatement createTable = (PreparedStatement) DBConnector.myConnection
-					.prepareStatement("CREATE TABLE IF NOT EXISTS SurvayTable(ID INT AUTO_INCREMENT, Notification VARCHAR(400), StartDate DATE NOT NULL, ExperationDate DATE NOT NULL, PRIMARY KEY(ID)");
+					.prepareStatement("CREATE TABLE IF NOT EXISTS Notification(ID INT AUTO_INCREMENT, Notification VARCHAR(400), StartDate DATE NOT NULL, ExperationDate DATE NOT NULL, PRIMARY KEY(ID)");
 			createTable.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void addToNotificationTable() {
+	/**
+	 * Adds new notification to the table.
+	 * @param notification
+	 * @param startDate
+	 * @param experationDate
+	 */
+	public void addToNotificationTable(String notification, Date startDate, Date experationDate) {
+		try {
+			PreparedStatement addNotification = (PreparedStatement) DBConnector.myConnection
+					.prepareStatement("INSERT INTO Notification VALUE(NULL,'" + notification + "',  '" + startDate + "','" + experationDate + "');");
+			addNotification.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
-	public void removeFromNotificationTable() {
+	/**
+	 * removes expired notification.
+	 * @param experationDate
+	 */
+	public void removeFromNotificationTable(Date experationDate) {
+		try {
+			PreparedStatement delete = (PreparedStatement) DBConnector.myConnection
+					.prepareStatement("DELETE FROM Notification WHERE ExperationDate ='"
+							+ experationDate + "';");
+			delete.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
