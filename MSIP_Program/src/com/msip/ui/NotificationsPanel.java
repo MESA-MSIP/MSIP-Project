99b28afc06cb3b0430c2f25b82b9f7d273b1448d
juanz <juanz@DarkMagic>
2016-04-06 23:59:28 -0700

    Add label to Notification table

--- MSIP_Program/src/com/msip/ui/NotificationsPanel.java
+++ MSIP_Program/src/com/msip/ui/NotificationsPanel.java
@@ -1,12 +1,8 @@
-/**
- * 
- */
 package com.msip.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
@@ -33,6 +29,7 @@ import javax.swing.table.DefaultTableModel;
 
 import com.msip.manager.MISPCore;
 import com.toedter.calendar.JDateChooser;
+import javax.swing.JLabel;
 
 /**
  * @author Celina
@@ -57,10 +54,6 @@ public class NotificationsPanel extends JPanel implements KeyListener {
 	public NotificationsPanel(MISPCore msipCore) {
 		this.manager = msipCore;
 
-		/**
-		 * Create the panel.
-		 */
-
 		setPreferredSize(new Dimension(700, 380));
 		setLayout(new BorderLayout(0, 0));
 
@@ -68,7 +61,9 @@ public class NotificationsPanel extends JPanel implements KeyListener {
 				new Object[][] {}, new String[] { "Notifications:",
 						"Start Date:", "Expiration Date:" }));
 		tableNotifications.setColumnSelectionAllowed(true);
-		tableNotifications.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tableNotifications.getTableHeader().setFont(GlobalUI.GlobalFont);
+		tableNotifications.setFont(GlobalUI.GlobalFont);
+		tableNotifications.setRowHeight(35);
 
 		tableNotifications.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent arg0) {
@@ -92,12 +87,14 @@ public class NotificationsPanel extends JPanel implements KeyListener {
 		add(verticalStrut, BorderLayout.NORTH);
 
 		JPanel panelNotificationInput = new JPanel();
-		panelNotificationInput.setPreferredSize(new Dimension(10, 100));
+		panelNotificationInput.setPreferredSize(new Dimension(10, 130));
 		add(panelNotificationInput, BorderLayout.SOUTH);
 
 		btnAdd = new JButton("Add");
 		btnAdd.setEnabled(false);
-		btnAdd.setBounds(307, 65, 75, 29);
+		btnAdd.setBounds(276, 85, 100, GlobalUI.BUTTONHEIGHT);
+		btnAdd.setFont(GlobalUI.GlobalFont);
+		btnAdd.setPreferredSize(new Dimension(100, GlobalUI.BUTTONHEIGHT));
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				addNotification();
@@ -110,15 +107,17 @@ public class NotificationsPanel extends JPanel implements KeyListener {
 		panelNotificationInput.add(btnAdd);
 
 		btnRemove = new JButton("Remove");
+		btnRemove.setFont(GlobalUI.GlobalFont);
+		btnRemove.setPreferredSize(new Dimension(100, GlobalUI.BUTTONHEIGHT));
 		btnRemove.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				deleteNotification();
-
 			}
 		});
-		btnRemove.setBounds(394, 65, 93, 29);
+		btnRemove.setBounds(396, 85, 100, GlobalUI.BUTTONHEIGHT);
 		panelNotificationInput.add(btnRemove);
 		textAreaNotifications = new JTextArea();
+		textAreaNotifications.setPreferredSize(new Dimension(4, 40));
 		textAreaNotifications.setWrapStyleWord(true);
 		textAreaNotifications.setLineWrap(true);
 		textAreaNotifications.addKeyListener(this);
@@ -136,14 +135,13 @@ public class NotificationsPanel extends JPanel implements KeyListener {
 					}
 
 				});
-		textAreaNotifications.setBounds(21, 6, 210, 48);
 		JScrollPane noteScrollPane = new JScrollPane(textAreaNotifications);
-		noteScrollPane.setBounds(21, 6, 210, 48);
+		noteScrollPane.setBounds(21, 35, 355, 39);
 		panelNotificationInput.add(noteScrollPane);
 
 		// Start Date
 		startDateChooser = new JDateChooser();
-		startDateChooser.setBounds(329, 6, 137, 26);
+		startDateChooser.setBounds(386, 35, 137, 39);
 		panelNotificationInput.add(startDateChooser);
 		// Adds todays date.
 		Date date1 = new Date();
@@ -160,11 +158,26 @@ public class NotificationsPanel extends JPanel implements KeyListener {
 				});
 
 		expirationDateChooser = new JDateChooser();
-		expirationDateChooser.setBounds(492, 6, 137, 26);
+		expirationDateChooser.setBounds(538, 35, 137, 39);
 		panelNotificationInput.add(expirationDateChooser);
 		// sets current date
 		Date date = new Date();
 		expirationDateChooser.setDate(date);
+		
+		JLabel lblEnterANew = new JLabel("Enter a new notification:");
+		lblEnterANew.setBounds(21, 0, 355, 39);
+		lblEnterANew.setFont(GlobalUI.LableFont);
+		panelNotificationInput.add(lblEnterANew);
+		
+		JLabel lblStartDate = new JLabel("Start Date:");
+		lblStartDate.setBounds(386, 0, 137, 39);
+		lblStartDate.setFont(GlobalUI.LableFont);
+		panelNotificationInput.add(lblStartDate);
+		
+		JLabel lblEndDate = new JLabel("End Date:");
+		lblEndDate.setBounds(538, 0, 137, 39);
+		lblEndDate.setFont(GlobalUI.LableFont);
+		panelNotificationInput.add(lblEndDate);
 
 		selectedExpirationDate = expirationDateChooser.getDate();
 		expirationDateChooser.getDateEditor().addPropertyChangeListener(
@@ -243,5 +256,4 @@ public class NotificationsPanel extends JPanel implements KeyListener {
 	public void keyPressed(KeyEvent e) {
 
 	}
-
 }
